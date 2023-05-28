2174c1f1f2e6cc4e93ae330bfcb92c52efa189b1
Ilya Gulya <ilyagulya@gmail.com>
2023-05-29 00:13:43 +0600

    Add go_router and use it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:ows_events_mobile/routing.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const HomePage());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: router,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   // This widget is the root of your application.
   @override
---
+++ lib/routing.dart
@@ -0,0 +1,6 @@
+import 'package:go_router/go_router.dart';
+import 'package:ows_events_mobile/main.dart';
+
+final router = GoRouter(
+  routes: [GoRoute(path: '/', builder: (context, state) => const HomePage())],
+);
