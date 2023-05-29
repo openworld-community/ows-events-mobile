d89eb435b1011ca147b5a4ae6fe9864efa60baf6
Ilya Gulya <ilyagulya@gmail.com>
2023-05-29 21:37:21 +0600

    Add dio, retrofit, logger

---
+++ lib/eventCell.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class EventCell extends StatelessWidget {
+  const EventCell({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
