0597e530337f34d498da4d33890484c676be2468
ArkadiiIl <odinls91@gmail.com>
2023-05-31 05:38:18 +0200

    Added EventListItem with example it in main, resources, assets

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:logger/logger.dart';
 import 'package:ows_events_mobile/features/events/data/EventsRepository.dart';
 import 'package:ows_events_mobile/features/events/data/api/EventsApi.dart';
 import 'package:ows_events_mobile/routing.dart';
+import 'package:ows_events_mobile/widgets/EventListItem.dart';
 
 final logger = Logger();
 
@@ -113,7 +114,17 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: ListView.builder(
         itemCount: 5,
-        itemBuilder: (context, index) {},
+        itemBuilder: (context, index) {
+          return EvenListItem(
+            title: 'Конференция «Как не умереть от эмигрантской тоски»',
+            description: 'Peredelano',
+            date: '21 мая, 20:00',
+            linkText: 'Вилла отцов разработки',
+            image: 'assets/images/test_image.jpg',
+            price: '500\$',
+            linkAction: () => {},
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
---
+++ lib/resources/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color mainTextColor = Color(0xff4e4e4e);
+  static const Color secondTextColor = Color(0xffacacac);
+  static const Color linkTextColor = Color(0xff5c9ad2);
+  static const Color accent = Color(0xff48c78e);
+}
