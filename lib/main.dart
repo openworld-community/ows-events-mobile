915ec870673ce00f9e609d598874770f44192733
Alexander <a.pride.xxi@tutanota.com>
2023-05-31 19:14:56 +0300

    Add topBar
    Добавил лого, однако отображается оно не корректно. Не могу понять во что его лучше поместить и пока самый подходящий вариант оказался объект Row.
    Создал Кнопку i, а так же Text рядом с ней, но сам текст уходит в длинну в сторону лого.
    Создал заголовок TopBar'а, отображается корректно.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
-import 'package:ows_events_mobile/features/events/data/EventsRepository.dart';
-import 'package:ows_events_mobile/features/events/data/api/EventsApi.dart';
 import 'package:ows_events_mobile/routing.dart';
 
-final logger = Logger();
-
 void main() async {
-  final dio = Dio();
-  final eventsApi = EventsApi(dio);
-  final eventsRepo = EventsRepository(eventsApi);
-
-  final events = await eventsRepo.getEvents();
-  logger.i(events);
-
   runApp(const MyApp());
 }
 
@@ -24,6 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
       routerConfig: router,
     );
   }
@@ -38,21 +27,6 @@ class HomePage extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -63,15 +37,6 @@ class HomePage extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -95,31 +60,27 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+            leading: Row(
+              children: [Image.asset('assets/logo.png')],
+            ),
+            title: const Text('Мероприятия'),
+            actions: [
+              Row(
+                children: [
+                  Text("30 дней до конца подписки"),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.info_outline_rounded, size: 35)),
+                ],
+              )
+            ],
+          )
+        ],
       ),
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) {},
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
