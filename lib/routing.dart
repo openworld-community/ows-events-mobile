1c54fd07e711275f9ea94efe908175a106285be3
Marianna Khanina <marianna.khanina@gmail.com>
2023-06-16 19:42:40 +0400

    #20 decompose main file

--- lib/routing.dart
+++
@@ -1,9 +0,0 @@
-import 'package:go_router/go_router.dart';
-import 'package:ows_events_mobile/main.dart';
-
-final router = GoRouter(
-  routes: [
-    GoRoute(path: '/', builder: (context, state) => const HomePage()),
-  ],
-);
-
