6088cde9ddcec0f041b97ece7ff98968ce79ad7a
Marianna Khanina <marianna.khanina@gmail.com>
2023-06-18 00:54:37 +0400

    #34 added event screen

--- lib/features/main/presentation/main_screen.dart
+++ lib/features/main/presentation/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ows_events_mobile/common_widgets/info_icon_button.dart';
 import 'package:ows_events_mobile/common_widgets/logo.dart';
+import 'package:ows_events_mobile/common_widgets/max_width_container.dart';
 import 'package:ows_events_mobile/features/events/presentation/events_screen.dart';
 
 class MainScreen extends StatelessWidget {
@@ -8,26 +9,17 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    final isSmallSizeScreen = screenSize.width <= 500;
-
-    return Container(
-      color: Theme.of(context).colorScheme.background,
-      child: Center(
-        child: SizedBox(
-          width: isSmallSizeScreen ? double.infinity : 500,
-          child: Scaffold(
-            appBar: AppBar(
-              leading: const Logo(),
-              leadingWidth: 150,
-              actions: const [
-                Text("30 дней"),
-                InfoIconButton(),
-              ],
-            ),
-            body: const EventsScreen(),
-          ),
+    return MaxWidthContainer(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: const Logo(),
+          leadingWidth: 150,
+          actions: const [
+            Text("30 дней"),
+            InfoIconButton(),
+          ],
         ),
+        body: const EventsScreen(),
       ),
     );
   }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static const double maxContainerWidth = 500;
+
   static ThemeData get theme => ThemeData(
         brightness: Brightness.light,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
