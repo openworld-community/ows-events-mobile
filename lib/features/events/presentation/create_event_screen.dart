1a8574149d68ff2cb2e17000dc681eacdaac022d
ArkadiiIl <odinls91@gmail.com>
2023-06-18 21:02:45 +0200

    refactor: refactored create_event_screen.dart

--- lib/features/events/presentation/create_event_screen.dart
+++ lib/features/events/presentation/create_event_screen.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ows_events_mobile/features/events/presentation/rounded_text_field.dart';
-
-import '../../../resources/colors.dart';
-import '../../../resources/text_styles.dart';
+import 'package:ows_events_mobile/resources/colors.dart';
+import 'package:ows_events_mobile/resources/text_styles.dart';
 
 class CreateEventScreen extends ConsumerStatefulWidget {
   const CreateEventScreen({super.key});
@@ -12,75 +11,84 @@ class CreateEventScreen extends ConsumerStatefulWidget {
   @override
   ConsumerState<ConsumerStatefulWidget> createState() =>
       _CreateEventScreenState();
-
 }
 
 class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
-  String countryText= '';
-  String cityText = '';
-  String timezoneText = '';
-  String nameText = '';
-  String descriptionText = '';
-  String startDateText = '';
-  String startTimeText = '';
-  String endDateText = '';
-  String endTimeText = '';
-  String priceText = '';
-  String linkText = '';
+  String _countryText = '';
+  String _cityText = '';
+  String _timezoneText = '';
+  String _nameText = '';
+  String _descriptionText = '';
+  String _startDateText = '';
+  String _startTimeText = '';
+  String _endDateText = '';
+  String _endTimeText = '';
+  String _priceText = '';
+  String _linkText = '';
 
   void _updateCountryText(String value) {
     setState(() {
-      countryText = value;
+      _countryText = value;
     });
   }
+
   void _updateCityText(String value) {
     setState(() {
-      cityText = value;
+      _cityText = value;
     });
   }
+
   void _updateTimezoneText(String value) {
     setState(() {
-      timezoneText = value;
+      _timezoneText = value;
     });
   }
+
   void _updateNameText(String value) {
     setState(() {
-      nameText = value;
+      _nameText = value;
     });
   }
+
   void _updateDescriptionText(String value) {
     setState(() {
-      descriptionText = value;
+      _descriptionText = value;
     });
   }
+
   void _updateStartDate(String value) {
     setState(() {
-      startDateText = value;
+      _startDateText = value;
     });
   }
+
   void _updateStartTimeText(String value) {
     setState(() {
-      startTimeText = value;
+      _startTimeText = value;
     });
   }
+
   void _updateEndDateText(String value) {
     setState(() {
-      endDateText = value;
+      _endDateText = value;
     });
   }
+
   void _updateEndTimeText(String value) {
     setState(() {
-      endTimeText = value;
+      _endTimeText = value;
     });
   }
+
   void _updatePriceText(String value) {
     setState(() {
-      priceText = value;
+      _priceText = value;
     });
   }
+
   void _updateLinkText(String value) {
     setState(() {
-      linkText = value;
+      _linkText = value;
     });
   }
 
@@ -94,7 +102,7 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
         child: SizedBox(
           width: isSmallSizeScreen ? double.infinity : 500,
           child: Scaffold(
-            appBar:  AppBar(
+            appBar: AppBar(
               centerTitle: true,
               title: const Text('Добавьте мероприятие'),
             ),
@@ -119,30 +127,27 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                               RoundedTextField(
                                 hintText: 'Страна',
                                 suffixIcon: Icons.arrow_drop_down_sharp,
-                                text: countryText,
+                                text: _countryText,
                                 onTextChanged: _updateCountryText,
                               ),
                               const SizedBox(height: 16),
                               RoundedTextField(
                                 hintText: 'Город',
                                 suffixIcon: Icons.arrow_drop_down_sharp,
-                                text: cityText,
+                                text: _cityText,
                                 onTextChanged: _updateCityText,
                               ),
                               const SizedBox(height: 16),
                               RoundedTextField(
                                 hintText: 'Часовой пояс',
                                 suffixIcon: Icons.arrow_drop_down_sharp,
-                                text: timezoneText,
+                                text: _timezoneText,
                                 onTextChanged: _updateTimezoneText,
                               )
                             ],
                           )
-                        ]
-                    ),
-
+                        ]),
                     const SizedBox(height: 24),
-
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -156,7 +161,7 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                           children: [
                             RoundedTextField(
                               hintText: 'Название',
-                              text: nameText,
+                              text: _nameText,
                               onTextChanged: _updateNameText,
                             ),
                             const SizedBox(height: 16),
@@ -164,16 +169,14 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                               hintText: 'Описание',
                               maxLines: 3,
                               minLines: 3,
-                              text: descriptionText,
+                              text: _descriptionText,
                               onTextChanged: _updateDescriptionText,
                             )
                           ],
                         )
                       ],
                     ),
-
                     const SizedBox(height: 24),
-
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -190,7 +193,7 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                                 hintText: 'дд.мм.гг',
                                 keyboardType: TextInputType.datetime,
                                 suffixIcon: Icons.calendar_today_outlined,
-                                text: startDateText,
+                                text: _startDateText,
                                 onTextChanged: _updateStartDate,
                               ),
                             ),
@@ -200,7 +203,7 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                                 hintText: '--:--',
                                 keyboardType: TextInputType.datetime,
                                 suffixIcon: Icons.access_time,
-                                text: startTimeText,
+                                text: _startTimeText,
                                 onTextChanged: _updateStartTimeText,
                               ),
                             ),
@@ -209,7 +212,6 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                       ],
                     ),
                     const SizedBox(height: 24),
-
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -226,7 +228,7 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                                 hintText: 'дд.мм.гг',
                                 keyboardType: TextInputType.datetime,
                                 suffixIcon: Icons.calendar_today_outlined,
-                                text: endDateText,
+                                text: _endDateText,
                                 onTextChanged: _updateEndDateText,
                               ),
                             ),
@@ -236,7 +238,7 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                                 hintText: '--:--',
                                 keyboardType: TextInputType.datetime,
                                 suffixIcon: Icons.access_time,
-                                text: endTimeText,
+                                text: _endTimeText,
                                 onTextChanged: _updateEndTimeText,
                               ),
                             ),
@@ -244,9 +246,7 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                         ),
                       ],
                     ),
-
                     const SizedBox(height: 24),
-
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -258,16 +258,15 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                         const SizedBox(height: 12),
                         RoundedTextField(
                           keyboardType: TextInputType.number,
-                          inputFormatters:
-                          [FilteringTextInputFormatter.digitsOnly],
-                          text: priceText,
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          text: _priceText,
                           onTextChanged: _updatePriceText,
                         )
                       ],
                     ),
-
                     const SizedBox(height: 24),
-
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -279,14 +278,12 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                         const SizedBox(height: 12),
                         RoundedTextField(
                           keyboardType: TextInputType.number,
-                          text: linkText,
+                          text: _linkText,
                           onTextChanged: _updateLinkText,
                         )
                       ],
                     ),
-
                     const SizedBox(height: 24),
-
                     ElevatedButton.icon(
                       icon: const Icon(Icons.image),
                       label: const Row(
@@ -299,102 +296,89 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                           // Дополнительные виджеты текста, если необходимо
                         ],
                       ),
-                      onPressed: () {  },
+                      onPressed: () {},
                       style: ButtonStyle(
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
                           RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(24.0),
                             side: const BorderSide(
-                                color: AppColors.input,
-                                width: 1.0
-                            ),
+                                color: AppColors.input, width: 1.0),
                           ),
                         ),
                         backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
+                            MaterialStateProperty.all<Color>(Colors.white),
                         foregroundColor:
-                        MaterialStateProperty.all<Color>(AppColors.input),
-                        maximumSize:
-                        MaterialStateProperty.all<Size>(const Size(200, 40)),
+                            MaterialStateProperty.all<Color>(AppColors.input),
+                        maximumSize: MaterialStateProperty.all<Size>(
+                            const Size(200, 40)),
                         elevation: MaterialStateProperty.all<double>(0),
                         padding: MaterialStateProperty.all<EdgeInsets>(
-                          const EdgeInsets.symmetric(vertical: 16, horizontal: 7),
+                          const EdgeInsets.symmetric(
+                              vertical: 16, horizontal: 7),
                         ),
                       ),
                     ),
-
                     const SizedBox(height: 24),
-
-                    Row(
-                        children: [
-                          TextButton(
-                            onPressed: (){
-                              Navigator.of(context).pop();
-                            },
-                            style:  ButtonStyle(
-                              shape: MaterialStateProperty
-                                  .all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(24.0),
-                                  side: const BorderSide(
-                                      color: AppColors.input,
-                                      width: 1.0
-                                  ),
-                                ),
-                              ),
-                              backgroundColor:
-                              MaterialStateProperty
-                                  .all<Color>(Colors.white),
-                              foregroundColor:
-                              MaterialStateProperty
-                                  .all<Color>(AppColors.mainTextColor),
-                              maximumSize:
-                              MaterialStateProperty.all<Size>(const Size(200, 40)),
-                              elevation: MaterialStateProperty.all<double>(0),
-                              padding: MaterialStateProperty.all<EdgeInsets>(
-                                const EdgeInsets
-                                    .symmetric(vertical: 16, horizontal: 7),
-                              ),
-
-                            ),
-                            child: const Text(
-                              'Отмена',
-                              style: AppTextStyles.inputTextStyle,
+                    Row(children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24.0),
+                              side: const BorderSide(
+                                  color: AppColors.input, width: 1.0),
                             ),
                           ),
-                          const SizedBox(width: 8),
-                          TextButton(
-                            onPressed: (){},
-                            style:  ButtonStyle(
-                              shape: MaterialStateProperty
-                                  .all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(24.0),
-                                  side: const BorderSide(
-                                      color: AppColors.input,
-                                      width: 1.0
-                                  ),
-                                ),
-                              ),
-                              backgroundColor:
-                              MaterialStateProperty.all<Color>(AppColors.accent),
-                              foregroundColor:
+                          backgroundColor:
                               MaterialStateProperty.all<Color>(Colors.white),
-                              maximumSize:
-                              MaterialStateProperty.all<Size>(const Size(200, 40)),
-                              elevation: MaterialStateProperty.all<double>(0),
-                              padding: MaterialStateProperty.all<EdgeInsets>(
-                                const EdgeInsets
-                                    .symmetric(vertical: 16, horizontal: 7),
-                              ),
+                          foregroundColor: MaterialStateProperty.all<Color>(
+                              AppColors.mainTextColor),
+                          maximumSize: MaterialStateProperty.all<Size>(
+                              const Size(200, 40)),
+                          elevation: MaterialStateProperty.all<double>(0),
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                            const EdgeInsets.symmetric(
+                                vertical: 16, horizontal: 7),
+                          ),
+                        ),
+                        child: const Text(
+                          'Отмена',
+                          style: AppTextStyles.inputTextStyle,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24.0),
+                              side: const BorderSide(
+                                  color: AppColors.input, width: 1.0),
                             ),
-                            child: const Text(
-                                'Сохранить',
-                                style: AppTextStyles.inputTextStyleWhite
-                            ),
-                          )
-                        ]
-                    ),
+                          ),
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              AppColors.accent),
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          maximumSize: MaterialStateProperty.all<Size>(
+                              const Size(200, 40)),
+                          elevation: MaterialStateProperty.all<double>(0),
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                            const EdgeInsets.symmetric(
+                                vertical: 16, horizontal: 7),
+                          ),
+                        ),
+                        child: const Text('Сохранить',
+                            style: AppTextStyles.inputTextStyleWhite),
+                      )
+                    ]),
                   ],
                 ),
               ),
--- lib/features/events/presentation/rounded_text_field.dart
+++ lib/features/events/presentation/rounded_text_field.dart
@@ -14,64 +14,53 @@ class RoundedTextField extends StatelessWidget {
   final VoidCallback? onPressedIcon;
   final String text;
   final Function(String) onTextChanged;
-  const RoundedTextField({
-    super.key,
-    this.hintText = '',
-    this.keyboardType = TextInputType.none,
-    this.inputFormatters,
-    this.maxLines = 1,
-    this.minLines = 1,
-    this.suffixIcon,
-    this.onPressedIcon,
-    required this.text,
-    required this.onTextChanged
 
-  });
+  const RoundedTextField(
+      {super.key,
+      this.hintText = '',
+      this.keyboardType = TextInputType.none,
+      this.inputFormatters,
+      this.maxLines = 1,
+      this.minLines = 1,
+      this.suffixIcon,
+      this.onPressedIcon,
+      required this.text,
+      required this.onTextChanged});
 
   @override
   Widget build(BuildContext context) {
-      return TextField(
-        style: AppTextStyles.inputTextStyle,
-        keyboardType: keyboardType,
-        maxLines: maxLines,
-        minLines: minLines,
-        inputFormatters: inputFormatters ?? [],
-        onChanged: (text) => {
-          onTextChanged(text)
-        },
-        decoration: InputDecoration(
-            hintText: hintText,
-            contentPadding: const EdgeInsets.fromLTRB(12, 8, 0, 8),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(24),
-            ),
-            focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(24.0),
-                borderSide: const BorderSide(
-                  color: AppColors.accent,
-                  width: 1.0,
+    return TextField(
+      style: AppTextStyles.inputTextStyle,
+      keyboardType: keyboardType,
+      maxLines: maxLines,
+      minLines: minLines,
+      inputFormatters: inputFormatters ?? [],
+      onChanged: (text) => {onTextChanged(text)},
+      decoration: InputDecoration(
+          hintText: hintText,
+          contentPadding: const EdgeInsets.fromLTRB(12, 8, 0, 8),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(24.0),
+              borderSide: const BorderSide(
+                color: AppColors.accent,
+                width: 1.0,
+              )),
+          suffixIcon: text.isNotEmpty
+              ? IconButton(
+                  onPressed: () {
+                    onTextChanged('');
+                  },
+                  icon: const Icon(Icons.cancel),
                 )
-            ),
-            suffixIcon:
-            text.isNotEmpty ?
-            IconButton(
-              onPressed: (){
-                onTextChanged('');
-              },
-              icon: const Icon(Icons.cancel),
-            ) :
-            suffixIcon != null &&
-                onPressedIcon != null ?
-            IconButton(
-                onPressed: onPressedIcon,
-                icon: Icon(suffixIcon)
-            ) :
-            suffixIcon != null ?
-            Icon(suffixIcon) :
-            null,
-            suffixIconColor: Colors.grey
-        ),
-      );
-    }
+              : suffixIcon != null && onPressedIcon != null
+                  ? IconButton(onPressed: onPressedIcon, icon: Icon(suffixIcon))
+                  : suffixIcon != null
+                      ? Icon(suffixIcon)
+                      : null,
+          suffixIconColor: Colors.grey),
+    );
   }
-
+}
