d9936ea312e303632e37db6e8a4234b2e331638a
OleksandraPetrova <96407924+leksidev@users.noreply.github.com>
2023-06-19 18:18:18 +0300

    feat: add a stub to the favorites screen

---
+++ lib/features/favourites/presentation/favourites_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FavouritesListScreen extends StatelessWidget {
+  const FavouritesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      child: Text('My favourites'),
+    );
+  }
+}
