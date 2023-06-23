57dbe5672164b9c84f86cc3f638590b518afef26
OleksandraPetrova <96407924+leksidev@users.noreply.github.com>
2023-06-23 21:55:53 +0300

    fix: change logo from png to svg, add flutter_svg package for this

--- lib/common_widgets/logo.dart
+++ lib/common_widgets/logo.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Logo extends StatelessWidget {
   const Logo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset('assets/logo.png');
+    return SvgPicture.asset('assets/peredelano-afisha.svg');
   }
 }
