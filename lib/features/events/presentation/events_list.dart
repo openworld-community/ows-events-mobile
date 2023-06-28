d1223ace5ed28cd4b6417c050cb1037ad7eced5d
Marianna Khanina <marianna.khanina@gmail.com>
2023-06-28 23:28:32 +0400

    #69 add stored data message

--- lib/features/events/presentation/events_list.dart
+++ lib/features/events/presentation/events_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ows_events_mobile/common_widgets/info_dialog.dart';
+import 'package:ows_events_mobile/common_widgets/offline_message.dart';
 import 'package:ows_events_mobile/common_widgets/refresh_indicator.dart';
 import 'package:ows_events_mobile/features/event/presentation/event_screen.dart';
 import 'package:ows_events_mobile/features/events/domain/event.dart';
@@ -23,34 +23,16 @@ class EventsList extends ConsumerWidget {
     return asyncEventsListData.when(
       data: (events) {
         final bool connectionError = controller.connectionError;
-        final String saveDateTime =
-            TimeUtils.formatDateTime(controller.saveDataTime);
+        String? offlineMessage;
 
         if (connectionError == true) {
-          Future.delayed(
-            Duration.zero,
-            () => showDialog(
-              context: context,
-              builder: (context) {
-                return InfoDialog(
-                  message: 'Оффлайн данные. Актуальны на момент $saveDateTime',
-                );
-              },
-            ),
-          );
+          final String saveDateTime =
+              TimeUtils.formatDateTime(controller.saveDataTime);
+          offlineMessage = 'Оффлайн данные. Актуальны на момент $saveDateTime';
         }
         return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              color: Theme.of(context).colorScheme.background,
-              padding: const EdgeInsets.only(top: 20),
-              width: double.infinity,
-              child: Text(
-                'Мероприятия',
-                style: Theme.of(context).textTheme.titleLarge,
-                textAlign: TextAlign.center,
-              ),
-            ),
             EventsFilters(
               onSearchTextChanged: (value) {
                 // TODO: добавить реализацию поиска по списку событий.
@@ -65,6 +47,10 @@ class EventsList extends ConsumerWidget {
                 throw UnimplementedError();
               },
             ),
+            if (offlineMessage != null)
+              OfflineMessage(
+                message: offlineMessage,
+              ),
             Expanded(
               child: AppRefreshIndicator(
                 onRefresh: () async {
