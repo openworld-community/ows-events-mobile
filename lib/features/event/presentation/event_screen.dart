101247cecae948eabb2801e05cf435e9b6c1692e
ArkadiiIl <odinls91@gmail.com>
2023-07-06 19:06:35 +0200

    feature/st_116 fixed event_screen.dart description

--- lib/features/event/presentation/event_screen.dart
+++ lib/features/event/presentation/event_screen.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:readmore/readmore.dart';
 import 'package:share_plus/share_plus.dart';
 
 import 'package:ows_events_mobile/common_widgets/max_width_container.dart';
@@ -55,76 +56,85 @@ class EventScreen extends ConsumerWidget {
                 )
               ],
             ),
-            body: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 176,
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      CachedNetworkImage(
-                        placeholder: (context, url) => const Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                        imageUrl: eventData.image.toString(),
-                        fit: BoxFit.fitWidth,
-                        errorWidget: (context, url, error) => Image.asset(
-                          'assets/image_event_placeholder.png',
+            body: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 176,
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: [
+                        CachedNetworkImage(
+                          placeholder: (context, url) => const Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                          imageUrl: eventData.image.toString(),
                           fit: BoxFit.fitWidth,
-                        ),
-                      ),
-                      Positioned(
-                        top: 12,
-                        left: 16,
-                        child: Container(
-                          padding: const EdgeInsets.fromLTRB(4, 10, 4, 10),
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.secondary,
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          child: Text(
-                            eventData.price.toString(),
-                            style: Theme.of(context).textTheme.bodyMedium,
+                          errorWidget: (context, url, error) => Image.asset(
+                            'assets/image_event_placeholder.png',
+                            fit: BoxFit.fitWidth,
                           ),
                         ),
-                      ),
-                    ],
+                        Positioned(
+                          top: 12,
+                          left: 16,
+                          child: Container(
+                            padding: const EdgeInsets.fromLTRB(4, 10, 4, 10),
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).colorScheme.secondary,
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Text(
+                              eventData.price.toString(),
+                              style: Theme.of(context).textTheme.bodyMedium,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  eventData.date.toString(),
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  '${eventData.location.country}, ${eventData.location.city}',
-                  style: Theme.of(context).textTheme.labelMedium?.copyWith(
-                        color: AppTheme.blue1,
-                      ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  eventData.description,
-                  style: Theme.of(context).textTheme.labelMedium?.copyWith(
-                        color: AppTheme.blue1,
-                      ),
-                ),
-                const SizedBox(height: 20),
-                Center(
-                  child: FavoriteButton(
-                    onPressed: toggleEventToFavorites,
-                    favorite: eventWithFavoriteMark.favoriteMark,
+                  const SizedBox(
+                    height: 20,
                   ),
-                ),
-              ],
+                  Text(
+                    eventData.date.toString(),
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    '${eventData.location.country}, ${eventData.location.city}',
+                    style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                          color: AppTheme.blue1,
+                        ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ReadMoreText(
+                    eventData.description,
+                    trimLines: 5,
+                    trimCollapsedText: 'trimCollapsedText'.tr(),
+                    trimExpandedText: 'trimExpandedText'.tr(),
+                    style: Theme.of(context).textTheme.bodyLarge,
+                    moreStyle: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          color: AppTheme.blue1,
+                        ),
+                    lessStyle: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          color: AppTheme.blue1,
+                        ),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: FavoriteButton(
+                      onPressed: toggleEventToFavorites,
+                      favorite: eventWithFavoriteMark.favoriteMark,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
