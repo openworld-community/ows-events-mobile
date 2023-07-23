7f78da849889f7de2e76039a5c0891f2d7471824
OleksandraPetrova <96407924+leksidev@users.noreply.github.com>
2023-07-23 22:30:15 +0300

    fix: fix warnings

--- lib/core/location_provider.dart
+++ lib/core/location_provider.dart
@@ -3,50 +3,46 @@ import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get_it/get_it.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:ows_events_mobile/core/position_manager.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
-final locationProvider = FutureProvider<String>(
-  (ref) async {
-    try {
-      final position = await _determinePosition();
-      final placemarksList = await placemarkFromCoordinates(
-        position.latitude,
-        position.longitude,
-      );
-      if (placemarksList.isNotEmpty) {
-        final placemark = placemarksList.first;
-        return '${placemark.country}'
-            '${placemark.locality == '' ? '' : ', ${placemark.locality}'}';
-      } else {
-        GetIt.I<Talker>().error(
-          'getLocationNotFoundError'.tr(),
-          Exception('placeMarksListIsEmptyError'.tr()),
-          StackTrace.current,
-        );
-        return 'locationNotFound'.tr();
-      }
-    } catch (error, stack) {
-      GetIt.I<Talker>().handle(
-        error,
-        stack,
-        'getLocationNotFoundError'.tr(),
-      );
-      return 'locationNotFound'.tr();
+final locationProvider = FutureProvider<String>((ref) async {
+  try {
+    final position = await PositionManager.determinePosition();
+    final placemarksList = await placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+    if (placemarksList.isNotEmpty) {
+      final placemark = placemarksList.first;
+      return '${placemark.country}'
+          '${placemark.locality == '' ? '' : ', ${placemark.locality}'}';
     }
-  },
-);
+    // logger.e(
+    //   'getLocationNotFoundError'.tr(),
+    //   Exception('placeMarksListIsEmptyError'.tr()),
+    //   StackTrace.current,
+    // );
+    return 'locationNotFound'.tr();
+  } catch (error, stack) {
+    GetIt.I<Talker>().handle(
+      error,
+      stack,
+      'getLocationNotFoundError'.tr(),
+    );
+    return 'locationNotFound'.tr();
+  }
+});
 
 final positionProvider = FutureProvider<Position?>((ref) async {
-  final logger = ref.read(loggerProvider);
-
   try {
     final position = await PositionManager.determinePosition();
     return position;
-  } catch (error) {
-    logger.e(
-      'getLocationNotFoundError'.tr(),
+  } catch (error, stack) {
+    GetIt.I<Talker>().handle(
       error,
-      StackTrace.current,
+      stack,
+      'getLocationNotFoundError'.tr(),
     );
   }
   return null;
