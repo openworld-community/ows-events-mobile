80ce5fb2989d7875acc9671cb01c415e9edf1382
OleksandraPetrova <96407924+leksidev@users.noreply.github.com>
2023-07-23 17:21:14 +0300

    fix: delete logger from location_provider and  events_dervice

--- lib/core/location_provider.dart
+++ lib/core/location_provider.dart
@@ -1,37 +1,40 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:ows_events_mobile/core/logger.dart';
+import 'package:get_it/get_it.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
-final locationProvider = FutureProvider<String>((ref) async {
-  final logger = ref.read(loggerProvider);
-  try {
-    final position = await _determinePosition();
-    final placemarksList = await placemarkFromCoordinates(
-      position.latitude,
-      position.longitude,
-    );
-    if (placemarksList.isNotEmpty) {
-      final placemark = placemarksList.first;
-      return '${placemark.country}'
-          '${placemark.locality == '' ? '' : ', ${placemark.locality}'}';
+final locationProvider = FutureProvider<String>(
+  (ref) async {
+    try {
+      final position = await _determinePosition();
+      final placemarksList = await placemarkFromCoordinates(
+        position.latitude,
+        position.longitude,
+      );
+      if (placemarksList.isNotEmpty) {
+        final placemark = placemarksList.first;
+        return '${placemark.country}'
+            '${placemark.locality == '' ? '' : ', ${placemark.locality}'}';
+      } else {
+        GetIt.I<Talker>().error(
+          'getLocationNotFoundError'.tr(),
+          Exception('placeMarksListIsEmptyError'.tr()),
+          StackTrace.current,
+        );
+        return 'locationNotFound'.tr();
+      }
+    } catch (error, stack) {
+      GetIt.I<Talker>().handle(
+        error,
+        stack,
+        'getLocationNotFoundError'.tr(),
+      );
+      return 'locationNotFound'.tr();
     }
-    logger.e(
-      'getLocationNotFoundError'.tr(),
-      Exception('placeMarksListIsEmptyError'.tr()),
-      StackTrace.current,
-    );
-    return 'locationNotFound'.tr();
-  } catch (error) {
-    logger.e(
-      'getLocationNotFoundError'.tr(),
-      error,
-      StackTrace.current,
-    );
-    return 'locationNotFound'.tr();
-  }
-});
+  },
+);
 
 Future<Position> _determinePosition() async {
   var permission = await Geolocator.requestPermission();
