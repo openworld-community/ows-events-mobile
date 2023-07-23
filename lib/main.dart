32b445bd9596ebf0158d4229d86e0e99c084348e
OleksandraPetrova <96407924+leksidev@users.noreply.github.com>
2023-07-23 16:57:16 +0300

    feat: add talker package for logging, add getIt for creare singleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
+import 'dart:async';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
 import 'package:ows_events_mobile/app.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
+  final talker = TalkerFlutter.init();
+  GetIt.I.registerSingleton(talker);
+  GetIt.I<Talker>().good('Talker started...');
 
-  runApp(EasyLocalization(
-    supportedLocales: const [Locale('ru')],
-    path: 'assets/translations',
-    fallbackLocale: const Locale('ru'),
-    child: const ProviderScope(
-      child: App(),
-    ),
-  ));
+  FlutterError.onError =
+      (details) => GetIt.I<Talker>().handle(details.exception, details.stack);
+
+  runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await EasyLocalization.ensureInitialized();
+      runApp(
+        EasyLocalization(
+          supportedLocales: const [Locale('ru')],
+          path: 'assets/translations',
+          fallbackLocale: const Locale('ru'),
+          child: const ProviderScope(
+            child: App(),
+          ),
+        ),
+      );
+    },
+    (error, stack) => GetIt.I<Talker>().handle(error, stack),
+  );
 }
