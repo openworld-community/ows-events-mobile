d3a285522296d026ee9bb01e05300914dd8abe6c
OleksandraPetrova <96407924+leksidev@users.noreply.github.com>
2023-07-23 17:21:55 +0300

    fix: delete logger from event_service

--- lib/features/events/application/events_service.dart
+++ lib/features/events/application/events_service.dart
@@ -1,6 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ows_events_mobile/core/logger.dart';
+import 'package:get_it/get_it.dart';
 import 'package:ows_events_mobile/features/events/data/events_local_store_repository.dart';
 import 'package:ows_events_mobile/features/events/data/events_provider.dart';
 import 'package:ows_events_mobile/features/events/domain/event.dart';
@@ -9,6 +9,7 @@ import 'package:ows_events_mobile/features/favorite_events/data/favorite_events_
 import 'package:ows_events_mobile/features/favorite_events/data/favotire_events_provider.dart';
 import 'package:ows_events_mobile/features/favorite_events/domain/event_with_favorite_mark.dart';
 import 'package:ows_events_mobile/features/favorite_events/domain/favorite_events.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 class EventsService {
   EventsService({
@@ -24,7 +25,6 @@ class EventsService {
   final FutureProvider<FavoriteEvents> favoriteEventsProvider;
   final FavoriteEventsRepository favoriteEventsRepository;
   final EventsLocalStoreRepository eventsLocalStoreRepository;
-  final Logger logger;
 
   late List<Event> _eventsList;
   late FavoriteEvents _favoriteEvents;
@@ -33,19 +33,24 @@ class EventsService {
 
   Future<List<EventWithFavoriteMark>> getEvents() async {
     try {
-      _eventsList =
-          await ref.read(eventsProvider.future).catchError((error) async {
-        connectionError = true;
-        final StoredEvents? storedEvents =
-            await eventsLocalStoreRepository.getEvents();
-        if (storedEvents != null) {
-          saveDataTime = storedEvents.saveTime;
-          return storedEvents.list;
-        }
-        throw Exception('$error. ${"getStoredEventsError".tr()}');
-      });
-    } catch (error) {
-      logger.e('getEventsError'.tr(), error, StackTrace.current);
+      _eventsList = await ref.read(eventsProvider.future).catchError(
+        (error) async {
+          connectionError = true;
+          final StoredEvents? storedEvents =
+              await eventsLocalStoreRepository.getEvents();
+          if (storedEvents != null) {
+            saveDataTime = storedEvents.saveTime;
+            return storedEvents.list;
+          }
+          throw Exception('$error. ${"getStoredEventsError".tr()}');
+        },
+      );
+    } catch (error, stack) {
+      GetIt.I<Talker>().handle(
+        error,
+        stack,
+        'getEventsError'.tr(),
+      );
       rethrow;
     }
 
@@ -53,11 +58,11 @@ class EventsService {
       _favoriteEvents = await ref.read(favoriteEventsProvider.future);
 
       return _mapMarksToEvents(_eventsList, _favoriteEvents);
-    } catch (error) {
-      logger.e(
-        'getFavoriteEventsIdsError'.tr(),
+    } catch (error, stack) {
+      GetIt.I<Talker>().handle(
         error,
-        StackTrace.current,
+        stack,
+        'getFavoriteEventsIdsError'.tr(),
       );
       rethrow;
     }
@@ -70,30 +75,37 @@ class EventsService {
       await favoriteEventsRepository.setFavoriteEvents(_favoriteEvents);
 
       return _mapMarksToEvents(_eventsList, _favoriteEvents);
-    } catch (error) {
-      logger.e('toggleFavoriteEventError'.tr(), error, StackTrace.current);
+    } catch (error, stack) {
+      GetIt.I<Talker>().handle(
+        error,
+        stack,
+        'toggleFavoriteEventError'.tr(),
+      );
       rethrow;
     }
   }
 
   List<EventWithFavoriteMark> _mapMarksToEvents(
       List<Event> eventsList, FavoriteEvents favoriteEvents) {
-    return eventsList.map((event) {
-      final bool favoriteMark = favoriteEvents.checkIdInList(event.id);
+    return eventsList.map(
+      (event) {
+        final bool favoriteMark = favoriteEvents.checkIdInList(event.id);
 
-      return EventWithFavoriteMark(
-        event: event,
-        favoriteMark: favoriteMark,
-      );
-    }).toList();
+        return EventWithFavoriteMark(
+          event: event,
+          favoriteMark: favoriteMark,
+        );
+      },
+    ).toList();
   }
 }
 
-final eventsServiceProvider = Provider<EventsService>((ref) => EventsService(
-      ref: ref,
-      eventsProvider: eventsProvider,
-      favoriteEventsProvider: favoriteEventsProvider,
-      favoriteEventsRepository: ref.read(favoriteEventsRepositoryProvider),
-      eventsLocalStoreRepository: ref.read(eventsLocalStoreRepositoryProvider),
-      logger: ref.read(loggerProvider),
-    ));
+final eventsServiceProvider = Provider<EventsService>(
+  (ref) => EventsService(
+    ref: ref,
+    eventsProvider: eventsProvider,
+    favoriteEventsProvider: favoriteEventsProvider,
+    favoriteEventsRepository: ref.read(favoriteEventsRepositoryProvider),
+    eventsLocalStoreRepository: ref.read(eventsLocalStoreRepositoryProvider),
+  ),
+);
