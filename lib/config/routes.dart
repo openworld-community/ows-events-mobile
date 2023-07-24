aeda8f166f8ac82a3ac21d0a487d02b74dee515f
OleksandraPetrova <96407924+leksidev@users.noreply.github.com>
2023-07-24 12:00:48 +0300

    feat: add rout for one event and plug talker_log on navigation

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,27 +1,37 @@
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
+import 'package:ows_events_mobile/features/event/presentation/event_screen.dart';
 import 'package:ows_events_mobile/features/favorite_events/presentation/favorite_events_screen.dart';
 import 'package:ows_events_mobile/features/main/presentation/main_screen.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 // Конфигурация GoRouter
 final routerConfig = GoRouter(
+  observers: [
+    TalkerRouteObserver(
+      GetIt.I<Talker>(),
+    ),
+  ],
   initialLocation: '/',
   routes: [
     GoRoute(
       name: 'mainScreen',
       path: '/',
       builder: (context, state) => const MainScreen(),
+      routes: [
+        GoRoute(
+          name: 'eventScreen',
+          path: ':id',
+          builder: (context, state) => EventScreen(
+            id: state.pathParameters['id']!,
+          ),
+        ),
+      ],
     ),
     GoRoute(
       name: 'favouriteEventsScreen',
       path: '/favourite',
       builder: (context, state) => const FavoriteEventsScreen(),
-    ),
-    // GoRoute(
-    //   name: 'eventScreen',
-    //   path: '/events/:id',
-    //   builder: (context, state) => EventScreen(
-    //     id: context.,
-    //   ),
     ),
   ],
 );
