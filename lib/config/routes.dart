968e44b8ca22ed10d372670880f1acdea2fdc7be
OleksandraPetrova <96407924+leksidev@users.noreply.github.com>
2023-07-25 12:37:10 +0300

    fix: delete favourites screen route

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,7 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ows_events_mobile/features/event/presentation/event_screen.dart';
-import 'package:ows_events_mobile/features/favorite_events/presentation/favorite_events_screen.dart';
 import 'package:ows_events_mobile/features/main/presentation/main_screen.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
@@ -34,11 +33,6 @@ final routerConfig = GoRouter(
           ],
         ),
       ],
-    ),
-    GoRoute(
-      name: 'favouritesEventsScreen',
-      path: 'favourite',
-      builder: (context, state) => const FavoriteEventsScreen(),
     ),
   ],
 );
