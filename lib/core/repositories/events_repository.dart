5e70359dad0df5d43d1393a8f3187a8abf77226e
Alexandra Petrova <96407924+leksidev@users.noreply.github.com>
2023-08-16 23:01:47 +0300

    fix: delete unused import

--- lib/core/repositories/events_repository.dart
+++ lib/core/repositories/events_repository.dart
@@ -4,7 +4,6 @@ import 'package:ows_events_mobile/features/events/domain/event.dart';
 import 'package:ows_events_mobile/features/events/domain/location.dart';
 import 'package:ows_events_mobile/core/network/network.dart';
 import 'package:ows_events_mobile/features/search/domain/search_parameter.dart';
-import 'package:ows_events_mobile/features/search/search_provider.dart';
 
 class EventsRepository {
   EventsRepository(this.api);
